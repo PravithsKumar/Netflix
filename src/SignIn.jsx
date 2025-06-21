90f790d3d200e507887a32d7a3253718c687ff93
BlackFoot <pmadiwal973@gmail.com>
2025-06-21 12:57:59 +0530

    one last

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -15,7 +15,7 @@ function SignIn() {
     e.preventDefault();
     try{
     const res=await
-    axios.post('http://localhost:5000/api/auth/login',{
+    axios.post('https://netflixbackend-wgic.onrender.com/api/auth/login',{
       email,
       password
     });
